255714007041951e1526b4b6b832c85fb1a9b205
joellekado <jkado@my.hpu.edu>
2021-01-06 14:34:59 -0600

    get route added

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated, rejectNonAdmin } = require('../modules/authentication-middleware');
+
 
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+// router.get('/', (req, res) => {
+//   res.sendStatus(200); // For testing only, can be removed
+// });
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+  console.log('/shelf GET route');
+  console.log('is authenticated?', req.isAuthenticated());
+  console.log('user', req.user);
+  let queryText = `SELECT * FROM "item" WHERE "user_id" = $1`;
+  
+  pool.query(queryText, [req.user.id]).then((result) => {
+      res.send(result.rows);
+  }).catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+  });
 });
+
 
 /**
  * Add an item for the logged in user to the shelf
